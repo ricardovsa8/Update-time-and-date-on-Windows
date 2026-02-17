595a85eb30e48315047f90331a02164e512ae3c6
Ricardo <140122993+ricardovsa8@users.noreply.github.com>
2026-02-16 19:42:21 -0500

    Script Update-time-and-date-on-Windows

---
+++ Update-time-and-date-on-Windows.ps1
@@ -0,0 +1,28 @@
+﻿#Modificar la hora y fecha
+#set-date -Date "20-5-2024 10:44:55 AM"
+
+#Ejecutar como administrador
+
+#Detener el servico de hora de windows
+Stop-Service w32time
+
+#Anular el registro y vuelva a registrar el servicio para restablecer la configuración prederminada
+w32tm /unregister
+w32tm /register
+
+#Iniciarl el servicio de hora en windows
+Start-Service w32time
+
+#Configurando parametro del servidor de hora
+$ServidorHora = "time.windows.com"
+
+#Configurar una fuente de hora externa
+w32tm /config /manualpeerlist:"$ServidorHora,0x1" /syncfromflags:manual /reliable:yes /update
+
+#Forma la sincronización de hora
+w32tm /resync /force
+
+#Obtener la hora actual
+#get-date
+
+#Autor Ricardo
